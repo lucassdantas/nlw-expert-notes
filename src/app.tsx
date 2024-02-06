037d15e7e66440316524994a2c67c067672a6185
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-06 20:22:41 -0300

    configuring overflow

--- src/app.tsx
+++ src/app.tsx
@@ -23,14 +23,27 @@ export function App() {
                         Grave uma nota em áudio que será convertida para texto automaticamente.
                     </p>
                 </div>
+          
+                <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+                    <span className='text-sm font-medium text-slate-300'>Há 2 dias</span>
+                   
+                    <p className='text-sm leading-6 text-slate-400'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas enim pariatur ratione voluptatem animi asperiores sint culpa voluptates optio qui ullam, architecto ipsam nam voluptatum rem accusamus odit placeat obcaecati!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas enim pariatur ratione voluptatem animi asperiores sint culpa voluptates optio qui ullam, architecto ipsam nam voluptatum rem accusamus odit placeat obcaecati!
+                    </p>
+
+                    <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0'/>
+                </div>
+          
+                <div className='rounded-md bg-slate-800 p-5 space-y-3'>
+                    <span className='text-sm font-medium text-slate-300'>Há 4 dias</span>
+                   
+                    <p className='text-sm leading-6 text-slate-400'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas enim pariatur ratione voluptatem animi asperiores sint culpa voluptates optio qui ullam, architecto ipsam nam voluptatum rem accusamus odit placeat obcaecati!
+                    </p>
+                </div>
                 
-                <div className='rounded-md bg-slate-800'>
-
-                </div>
-
-                <div className='rounded-md bg-slate-800'>
-
-                </div>
+                
 
             </div>
         </div>
