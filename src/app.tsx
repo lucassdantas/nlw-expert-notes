0fe959dc23a8b8437a8c35f8d38677af608b556e
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-06 20:28:55 -0300

    passing notes to components

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import logo from './assets/logo-nwl-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
 
 export function App() {
     return(
@@ -16,35 +18,10 @@ export function App() {
 
             <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
                 
-                <div className='rounded-md bg-slate-700 p-5 space-y-3'>
-                    <span className='text-sm font-medium text-slate-200'>Adicionar nota</span>
-                   
-                    <p className='text-sm leading-6 text-slate-400'>
-                        Grave uma nota em áudio que será convertida para texto automaticamente.
-                    </p>
-                </div>
-          
-                <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
-                    <span className='text-sm font-medium text-slate-300'>Há 2 dias</span>
-                   
-                    <p className='text-sm leading-6 text-slate-400'>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas enim pariatur ratione voluptatem animi asperiores sint culpa voluptates optio qui ullam, architecto ipsam nam voluptatum rem accusamus odit placeat obcaecati!
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas enim pariatur ratione voluptatem animi asperiores sint culpa voluptates optio qui ullam, architecto ipsam nam voluptatum rem accusamus odit placeat obcaecati!
-                    </p>
-
-                    <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0'/>
-                </div>
-          
-                <div className='rounded-md bg-slate-800 p-5 space-y-3'>
-                    <span className='text-sm font-medium text-slate-300'>Há 4 dias</span>
-                   
-                    <p className='text-sm leading-6 text-slate-400'>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas enim pariatur ratione voluptatem animi asperiores sint culpa voluptates optio qui ullam, architecto ipsam nam voluptatum rem accusamus odit placeat obcaecati!
-                    </p>
-                </div>
-                
-                
-
+                <NewNoteCard/>
+                <NoteCard/>
+                <NoteCard/>
+                <NoteCard/>
             </div>
         </div>
     )
