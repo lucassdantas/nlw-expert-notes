66b47d9dd7e3a585820434da7205ef84019855cc
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-07 18:16:48 -0300

    adding style to modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,7 @@
 import * as Dialog  from '@radix-ui/react-dialog'
 import {formatDistanceToNow} from 'date-fns'
 import {ptBR} from 'date-fns/locale'
+import {X} from 'lucide-react'
 interface NoteCardProps {
     note:{
         date: Date 
@@ -22,7 +23,10 @@ export function NoteCard({note}:NoteCardProps) {
         </Dialog.Trigger>
         <Dialog.Portal>
             <Dialog.Overlay className='inset-0 fixed bg-black/50'></Dialog.Overlay>
-            <Dialog.Content className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-700 max-w-[640px] h-[60vh] w-full rounded-md flex flex-col outline-none'>
+            <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-700 max-w-[640px] h-[60vh] w-full rounded-md flex flex-col outline-none'>
+
+                <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'> <X className='size-5'/> </Dialog.Close>
+
                 <div className='flex flex-1 flex-col gap-3 p-5 '>
                     <span className='text-sm font-medium text-slate-300'>{formatDistanceToNow(note.date, {locale:ptBR, addSuffix:true})}</span>
 
@@ -30,8 +34,8 @@ export function NoteCard({note}:NoteCardProps) {
                         {note.content}
                     </p>
                 </div>
-                <button type='button' className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none'>
-                        Deseja apagar essa nota?
+                <button type='button' className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'>
+                        Deseja <span className='text-red-400 group-hover:underline'>apagar essa nota?</span>
                 </button>
             </Dialog.Content>
         </Dialog.Portal>
