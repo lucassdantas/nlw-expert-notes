edd0a8c7fdb02a7c245019f4fd1c077350ab7c5d
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-07 18:10:38 -0300

    adding dialog to project

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,10 @@ import logo from './assets/logo-nwl-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+const note = {
+    date: new Date(),
+    content: 'teste'
+}
 export function App() {
     return(
         <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -19,9 +23,8 @@ export function App() {
             <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
                 
                 <NewNoteCard/>
-                <NoteCard/>
-                <NoteCard/>
-                <NoteCard/>
+                <NoteCard note={note}/>
+                <NoteCard note={note}/>
             </div>
         </div>
     )
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,17 +1,40 @@
+import * as Dialog  from '@radix-ui/react-dialog'
+import {formatDistanceToNow} from 'date-fns'
+import {ptBR} from 'date-fns/locale'
+interface NoteCardProps {
+    note:{
+        date: Date 
+        content: string
+    }
+}
 
-export function NoteCard() {
+export function NoteCard({note}:NoteCardProps) {
  return(
-    <button className='
-        rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'
-    >
-        <span className='text-sm font-medium text-slate-300'>Há 4 dias</span>
-        
-        <p className='text-sm leading-6 text-slate-400'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas enim pariatur ratione voluptatem animi asperiores sint culpa voluptates optio qui ullam, architecto ipsam nam voluptatum rem accusamus odit placeat obcaecati!
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas enim pariatur ratione voluptatem animi asperiores sint culpa voluptates optio qui ullam, architecto ipsam nam voluptatum rem accusamus odit placeat obcaecati!
-        </p>
+    <Dialog.Root>
+        <Dialog.Trigger className='
+            rounded-md text-left flex-col bg-slate-800 p-5 gap-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'
+        >
+            <span className='text-sm font-medium text-slate-300'>{note.date.toISOString()}</span>
+            <p className='text-sm leading-6 text-slate-400'>
+                {note.content}
+            </p>
+            <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+            <Dialog.Overlay className='inset-0 fixed bg-black/50'></Dialog.Overlay>
+            <Dialog.Content className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-700 max-w-[640px] h-[60vh] w-full rounded-md flex flex-col outline-none'>
+                <div className='flex flex-1 flex-col gap-3 p-5 '>
+                    <span className='text-sm font-medium text-slate-300'>{formatDistanceToNow(note.date, {locale:ptBR, addSuffix:true})}</span>
 
-        <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
-    </button>
+                    <p className='text-sm leading-6 text-slate-400'>
+                        {note.content}
+                    </p>
+                </div>
+                <button type='button' className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none'>
+                        Deseja apagar essa nota?
+                </button>
+            </Dialog.Content>
+        </Dialog.Portal>
+    </Dialog.Root>
  )   
 }
